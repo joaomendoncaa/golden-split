baca842bfaf2a1ae6c280e2fec6f821b80410c40
joaomendoncaa <joaommm0312@gmail.com>
2023-05-19 14:23:42 +0100

    split

--- src/lib/split.ts
+++ src/lib/split.ts
@@ -1,3 +1,28 @@
 import * as vscode from "vscode";
 
-export function goldenSplitEditorGroup(): void {}
+function getActiveEditorDimensions(): { width: number; height: number } | undefined {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return undefined;
+    }
+
+    const visibleRange = editor.visibleRanges[0];
+    const fontSize = vscode.workspace.getConfiguration("editor").get("fontSize") as number;
+    const numVisibleColumns = visibleRange.end.character - visibleRange.start.character + 1;
+    const typicalHalfwidthCharacterWidth = vscode.workspace.getConfiguration("editor");
+    console.log("typicalHalfwidthCharacterWidth", typicalHalfwidthCharacterWidth);
+    const lineHeight = vscode.workspace.getConfiguration("editor").get("lineHeight") as number;
+    const width = numVisibleColumns * fontSize;
+    const height = editor.visibleRanges.reduce(
+        (acc, range) => acc + lineHeight * (range.end.line - range.start.line + 1),
+        0
+    );
+
+    return { width, height };
+}
+
+export function goldenSplitEditorGroup(): void {
+    let dimensions = getActiveEditorDimensions();
+
+    console.log("dimensions", dimensions);
+}
